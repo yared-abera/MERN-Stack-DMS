8c27c3c8fd99af7c544fa607a6bfeb8e306b4b24
yared-abera <yaredoabera7@gmail.com>
2025-02-18 21:13:42 +0300

    modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ import RegisterStudent from "./pages/proctorManager/RegisterStudentPage";
 import ViewFeedback from "./pages/proctorManager/ViewFeedbackPage";
 import ViewMaintenance from "./pages/proctorManager/ViewMaintenancePage";
 import GenerateReport from "./pages/proctorManager/GenerateReportPage";
-import StudentDashboard from "./components/student/layout"
+import StudentDashboard from "./components/proctor/layout"
 function App() {
   const theme = useSelector((state) => state.theme.mode);
   useEffect(() => {
--- client/src/components/admin/layout.jsx
+++ client/src/components/admin/layout.jsx
@@ -10,11 +10,10 @@ export default function AdminLayout() {
     <SidebarProvider  >
       <SideBarComponent />
       <main className=" w-full flex ">
-        <div className=" fixed mt-8 z-20  h-7   mr-0">
-    
+        <div className=" fixed mt-8 z-20  h-7   mr-0">   
           <SidebarTrigger />
         </div>
-
+        
         <div className=" flex-1 flex flex-col gap-0.5 ml-4  w-full relative ">
           <Header />
           <Outlet className="flex-1 " />
