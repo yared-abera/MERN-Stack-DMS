c21c6d4971ae1e2c9e5165366f4e95b94ffa790a
yared-abera <yaredoabera7@gmail.com>
2025-02-27 00:51:54 +0300

    registerBlockPageModified

--- client/src/pages/proctorManager/RegisterBlockPage.jsx
+++ client/src/pages/proctorManager/RegisterBlockPage.jsx
@@ -1,37 +1,56 @@
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
 import {RegisterBlock} from "@/config/data"
 import CommonForm from "@/components/common/form"
+import { motion } from "framer-motion";
+import { useState } from "react";
+const initialFormData=
+  {
+    blockNum : "",
+    capacity: " ",
+    foundIn:" ",
+    status:" ",
+    totalRoom:" ",
+    availableRoom: " ",
+    isSelectedForSpecialStud:" ",
+    isSelectedForDisableStud: " ",
+
+  }
+
+
 const RegisterBlockComp = () => {
-  
+  const[formData,setFormData]=useState(initialFormData);
+  const onSubmit=(event)=>{
+     event.preventDefault();
+  }
 
-
+  function isFormValid() {
+    return Object.values(formData).every((item) => item !== "");
+  }
     return (
       <div className="border-2 border-blue-600 h-full">
-       <div>
+         <motion.div
+                      initial={{ x: "+100%" }}
+                      animate={{ x: 0 }}
+                      exit={{ x: "-100%" }}
+                      transition={{ duration: 0.99, ease: "easeOut" }}
+                      className=" m-auto md:w-1/2 p-6 rounded-2xl shadow-lg "
+                    >  
       <Card >
         <CardHeader>
-          <CardTitle>Register Block</CardTitle>
+          <CardTitle>Fill the form for Register Block</CardTitle>
         </CardHeader>
         <CardContent>
-          <form className="space-y-4">
-            <Input placeholder="Block Name" />
-            <Input placeholder="Location" />
-            <Input type="number" placeholder="Capacity" />
-            <Button type="submit">Register Block</Button>
-          </form>
          <CommonForm
-            formControls={}
-            formData={}
-            setFormData={}
-            onSubmit={}
-            buttonText={}
-            isBtnDisabled={}
+            formControls={RegisterBlock}
+            formData={formData}
+            setFormData={setFormData}
+            onSubmit={onSubmit}
+            buttonText={"Register Block"}
+            isBtnDisabled={!isFormValid()}
           />
         </CardContent>
       </Card>
-      </div>
+       </motion.div>
       </div>
     );
   };
