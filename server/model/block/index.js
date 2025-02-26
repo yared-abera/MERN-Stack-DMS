ae57143b7b24dc82064e3acd2ac0fd89cc6867ca
yared-abera <yaredoabera7@gmail.com>
2025-02-27 00:56:02 +0300

    block model modified

--- server/model/block/index.js
+++ server/model/block/index.js
@@ -2,7 +2,14 @@ const mongoose = require("mongoose");
 
 const blockSchema = new mongoose.Schema({
     blockNum: { type: Number,required: true },
-    proctorId: { type: mongoose.Schema.Types.ObjectId, ref: "Proctor", required: true },
+    capacity:{type:Number},
+    foundIn:{type:String,enum:["maleArea","femaleArea"]},
+    status:{type:String,enum: ["Available", "Occupied"],default:"Available"},
+    totalRoom:{type:Number,min:1} ,
+    availableRoom:{type:Number } ,
+    isSelectedForSpecialStud:{type:Boolean,default:false } ,
+    isSelectedForDisableStud:{type:Boolean,default:false } ,
 });
+
 
 module.exports = mongoose.model("Block", blockSchema);
