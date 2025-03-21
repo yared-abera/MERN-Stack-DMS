d4b240c02b046159bce01e246d93858d2d10d9d8
xulkif <zulkifazher2@gmail.com>
2025-03-21 23:56:29 +0300

    deanLayout

--- client/src/pages/studentDean/index.jsx
+++ client/src/pages/studentDean/index.jsx
@@ -1,36 +1,73 @@
 import { useDispatch } from "react-redux";
-import img from "../../assets/img/gibi.jpg";
-import logo from "../../assets/img/University_logo.png";
+import logo from "../../assets/img/University_logo.png"; // Using 'logo' consistently
+import img2 from "../../assets/img/gibi.jpg";
+import img3 from "../../assets/img/gibi4.jpg";
+import img4 from "../../assets/img/gibi6.jpg";
+import img5 from "../../assets/img/stud.jpg";
+import img6 from "../../assets/img/graugate.jpg";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { useEffect, useState } from "react";
+
 import { Button } from "@/components/ui/button";
-import { useEffect } from "react";
+
 import { getAllBlock } from "@/store/blockSlice";
 import StudentInfoChart from "@/components/studentDean/indexGraph";
 import { getAllocatedStudent } from "@/store/studentAllocation/allocateSlice";
 
 export default function StudentDeanHome() {
   const dispatch = useDispatch();
-  
+
   useEffect(() => {
     dispatch(getAllBlock());
     dispatch(getAllocatedStudent());
   }, [dispatch]);
 
+  const Images = [img2, img3, img4, img5, img6];
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // Automatic slideshow effect
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prev) => (prev + 1) % Images.length);
+    }, 10000);
+
+    return () => clearInterval(interval);
+  },);
+  const handleNext = () => {
+    setCurrentIndex((prev) => (prev + 1) % Images.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prev) => (prev - 1 + Images.length) % Images.length);
+  };
+
   return (
     <div className="w-full min-h-screen flex flex-col mt-20">
-      
       {/* Header with background image covering 30% of viewport height */}
-      <div 
-        className="w-full h-[50vh] flex justify-center mt-3"
+      <div
+        className="w-full h-[60vh] flex justify-center mt-3"
         style={{
-          backgroundImage: `url(${img})`,
+          backgroundImage: `url(${Images[currentIndex]})`,
           backgroundPosition: "center",
           backgroundSize: "cover",
         }}
       >
-        <div >
-        <img src={logo} alt="University Logo" className="max-h-full z-20" />
+        <button
+          onClick={handlePrev}
+          className="absolute bg-black left-4 top-1/2 -translate-y-1/2 bg-white/75 p-2 rounded-full hover:bg-white/50"
+        >
+          <ChevronLeft className="w-5 h-5 md:w-8 md:h-8" />
+        </button>
+
+        <button
+          onClick={handleNext}
+          className="absolute right-2 top-1/2 -translate-y-1/2 bg-white/75 p-2 rounded-full hover:bg-white/50"
+        >
+          <ChevronRight className="w-5 h-5 md:w-8 md:h-8" />
+        </button>
+        <div>
+          <img src={logo} alt="University Logo" className="max-h-full z-20" />
         </div>
-        
       </div>
 
       {/* Main Content */}
@@ -38,7 +75,9 @@ export default function StudentDeanHome() {
         <div className="flex flex-col gap-4">
           <div className="bg-white p-4 rounded shadow">
             <div className="text-center">
-          <h1 className="text-lg md:text-xl font-bold ">Student Information </h1>
+              <h1 className="text-lg md:text-xl font-bold ">
+                Student Information{" "}
+              </h1>
             </div>
             <StudentInfoChart />
           </div>
@@ -99,9 +138,7 @@ export default function StudentDeanHome() {
       </div>
 
       {/* Footer */}
-      <div className="w-full bg-orange-500 text-center p-4">
-        Footer
-      </div>
+      <div className="w-full bg-orange-500 text-center p-4">Footer</div>
     </div>
   );
 }
