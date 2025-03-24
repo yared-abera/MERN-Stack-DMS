c019e14609b162db13ffa32ff65e59c760b760f9
xulkif <zulkifazher2@gmail.com>
2025-03-24 10:46:32 +0300

    student server

--- server/router/student/studentRoute.js
+++ server/router/student/studentRoute.js
@@ -1,9 +1,10 @@
 const express=require('express')
-const { InsertStudent,fetchAllStudent } = require('../../controller/Student/studentController')
+const { InsertStudent,fetchAllStudent,fetchSingleStudent } = require('../../controller/Student/studentController')
 
 const route=express.Router()
 
 route.post('/insert',InsertStudent)
 route.get('/get',fetchAllStudent)
+route.get('/getOne/:id',fetchSingleStudent)
 
 module.exports=route
